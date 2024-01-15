1583cda253079634ac942d61198c6fde3b750c2e
Fabian <fb.conis@gmail.com>
2024-01-15 11:31:55 -0500

    Mensaje del commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ $title }}</title>
+        @vite(['resources/css/app.css','resources/js/app.js'])
+    </head>
+    <body class = " antialiased bg-slate-100 dark:bg-slate-900 ">
+        <x-layouts.navigation/>
+        @if(session('status'))
+            <div class = "max-w-screen-xl px-3 py-2 mx-auto font-bold text-white sm:px-6 lg:px-8 bg-emerald-500 dark:bg-emerald-700">
+                {{ session('status') }}
+            </div>
+        @endif
+        {{ $slot }}
+    </body>
+</html>
